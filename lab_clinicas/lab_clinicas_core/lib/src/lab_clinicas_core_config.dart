d73755ce1b834bb9a2d9898e6ba528b614017a03
gregorio <igregoriodoneto@outlook.com>
2024-02-04 10:42:27 -0300

    feat: config core

---
+++ lab_clinicas/lab_clinicas_core/lib/src/lab_clinicas_core_config.dart
@@ -0,0 +1,46 @@
+import 'package:asyncstate/asyncstate.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_getit/flutter_getit.dart';
+import 'package:lab_clinicas_core/src/loader/lab_clinicas_loader.dart';
+
+class LabClinicasCoreConfig extends StatelessWidget {
+  final ApplicationBindings? bindings;
+  final List<FlutterGetItPageRouter>? pages;
+  final List<FlutterGetItPageBuilder>? pagesBuilders;
+  final List<FlutterGetItModule>? modules;
+  final String title;
+
+  LabClinicasCoreConfig({
+    super.key,
+    this.bindings,
+    this.pages,
+    this.pagesBuilders,
+    this.modules,
+    required this.title,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FlutterGetIt(
+      debugMode: kDebugMode,
+      bindings: bindings,
+      pages: [...pages ?? [], ...pagesBuilders ?? []],
+      modules: modules,
+      builder: (context, routes, flutterGetItNavObserver) {
+        return AsyncStateBuilder(
+          loader: LabClinicasLoader(),
+          builder: (navigatorObserver) {
+            return MaterialApp(
+              navigatorObservers: [
+                flutterGetItNavObserver,
+              ],
+              routes: routes,
+              title: title,
+            );
+          },
+        );
+      },
+    );
+  }
+}
