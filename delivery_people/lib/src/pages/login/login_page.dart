1b55212bf3480789021dcdadbaa7aad1a369ac28
gregorio <igregoriodoneto@outlook.com>
2024-02-08 09:46:34 -0300

    feat: button

--- delivery_people/lib/src/pages/login/login_page.dart
+++ delivery_people/lib/src/pages/login/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:delivery_people/src/utils/colors.dart';
 import 'package:delivery_people/src/wigets/input_custom_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -21,56 +22,101 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(24),
-        child: Center(
-          child: Column(
-            children: [
-              SizedBox(height: 80),
-              Container(
-                child: Image.asset('assets/images/pigz-logotipo-branco.png'),
-              ),
-              SizedBox(height: 8),
-              Text(
-                'Para Entregadores',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontFamily: 'Poppins',
-                  fontWeight: FontWeight.bold,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(24),
+          child: Center(
+            child: Column(
+              children: [
+                SizedBox(height: 80),
+                Container(
+                  child: Image.asset('assets/images/pigz-logotipo-branco.png'),
                 ),
-              ),
-              SizedBox(height: 37),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Text(
-                    'Login',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontFamily: 'Poppins',
-                      fontWeight: FontWeight.bold,
+                SizedBox(height: 8),
+                Text(
+                  'Para Entregadores',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 37),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Login',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 16),
+                // Email
+                InputCustomWidget(
+                  label: 'Email',
+                  hintText: 'example@pigz.com.br',
+                  obscureText: false,
+                  toggleViewPassword: () {},
+                ),
+                SizedBox(height: 20),
+                // Senha
+                InputCustomWidget(
+                  label: 'Senha',
+                  hintText: '******',
+                  isPassword: true,
+                  obscureText: obscureText,
+                  toggleViewPassword: isObscureText,
+                ),
+        
+                SizedBox(height: 16),
+                // esqueci a senha
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Esqueci minha senha',
+                        style: TextStyle(
+                          color: AppColors.gray2,
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+        
+                SizedBox(height: 16),
+                // Button entrar
+                SizedBox(
+                  height: 48,
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16),
+                        side: BorderSide(color: AppColors.orangeLight),
+                      ),
+                      primary: AppColors.orange,
+                    ),
+                    onPressed: () {},
+                    child: Text(
+                      'Entrar',
+                      style: TextStyle(
+                        color: AppColors.white,
+                        fontSize: 16,
+                        fontFamily: 'Poppins',
+                      ),
                     ),
                   ),
-                ],
-              ),
-              SizedBox(height: 16),
-              // Email
-              InputCustomWidget(
-                label: 'Email',
-                hintText: 'example@pigz.com.br',
-                obscureText: false,
-                toggleViewPassword: () {},
-              ),
-              SizedBox(height: 20),
-              // Senha
-              InputCustomWidget(
-                label: 'Senha',
-                hintText: '******',
-                isPassword: true,
-                obscureText: obscureText,
-                toggleViewPassword: isObscureText,
-              ),
-            ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
