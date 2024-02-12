b4983b2915bf176d3fa69ff6c658f7cdc2eaa303
gregorio <igregoriodoneto@outlook.com>
2024-02-12 10:27:23 -0300

    feat: creating user repository

--- delivery_people/lib/src/core/repositories/user_repository.dart
+++ delivery_people/lib/src/core/repositories/user_repository.dart
@@ -1,3 +1,43 @@
+import 'package:delivery_people/src/core/db/mock.dart';
+import 'package:delivery_people/src/core/models/user.dart';
+import 'package:dio/dio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 class UserRepository {
+  late Dio dio;
+
+  UserRepository({required this.dio});
+
+  // Login user
+  Future<bool> login(String email, String password) async {
+    try {
+      // Procura usuário na lista de mocks
+      User? user = mockUsers.firstWhere((user) => user['email'] == email, orElse: () => null);
+      if (user != null) {
+        SharedPreferences prefs = await SharedPreferences.getInstance();
+        await prefs.setString('user_email', user.email);
+        return true;
+      } else {
+        return false;
+      }
+    } catch(e) {
+      throw Exception('Error when logging in: ${e}');
+    }
+  }
+
+  Future<bool> logout() async {
+    try {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      if (prefs.getString('user_email') != '') {
+        prefs.setString('user_email', '');
+        return true;
+      } else {
+        return false;
+      }
+    } catch(e) {
+      throw Exception('Not logged out');
+    }
+  }
+
 
 }
