99ea866d10ee139aa7b58d3f2bc67b233edf74c1
igregorioneto <igregoriodoneto@outlook.com>
2024-03-14 16:32:31 -0300

    feat: creating project notes with riverpod, configuratin initial, creating themes

---
+++ notes_with_riverpod/lib/app.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:notes_with_riverpod/pages/notes_page.dart';
+import 'package:notes_with_riverpod/theme/custom_theme.dart';
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Notes With Riverpod",
+      theme: ThemeData(
+        primarySwatch: CustomTheme.black,
+        scaffoldBackgroundColor: Colors.white,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: CustomTheme.elevatedButtonStyle,
+        ),
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: CustomTheme.black,
+        ),
+      ),
+      home: const NotesPage(),
+    );
+  }
+}
---
+++ notes_with_riverpod/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:notes_with_riverpod/app.dart';
+
+void main() {
+  runApp(const ProviderScope(child: App()));
+}
