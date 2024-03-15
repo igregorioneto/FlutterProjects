9b185e5a0ee29c165d857aa9a4ec6c20cb22e322
igregorioneto <igregoriodoneto@outlook.com>
2024-03-15 16:08:25 -0300

    feat: empty notes

---
+++ notes_with_riverpod/lib/widgets/empty_notes.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:notes_with_riverpod/providers/notes/notes_view_options.dart';
+import 'package:notes_with_riverpod/providers/notes/view_filtered_provider.dart';
+
+class EmptyNotes extends StatelessWidget {
+  const EmptyNotes({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer(
+      builder: (context, ref, _) {
+        final viewOption = ref.watch(filterNotesProvider).value;
+        final String noteType = viewOption == NotesViewOptions.archived
+            ? 'Nenhuma nota arquivada.'
+            : 'Crie sua primeira nota!';
+
+        return Column(
+          children: [
+            Image.asset('assets/empty_notes.png'),
+            Text(
+              noteType,
+              style: Theme.of(context).textTheme.subtitle1,
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
