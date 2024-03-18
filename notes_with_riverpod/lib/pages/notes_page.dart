2022c2344535670eca8e39574321faa1ad5023a1
igregorioneto <igregoriodoneto@outlook.com>
2024-03-18 09:58:31 -0300

    feat: notes page

--- notes_with_riverpod/lib/pages/notes_page.dart
+++ notes_with_riverpod/lib/pages/notes_page.dart
@@ -1,22 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:notes_with_riverpod/providers/notes/notes_view_options.dart';
+import 'package:notes_with_riverpod/providers/notes/view_filtered_provider.dart';
+import 'package:notes_with_riverpod/providers/notes/view_toggle_provider.dart';
+import 'package:notes_with_riverpod/repositories/notes_repository.dart';
+import 'package:notes_with_riverpod/widgets/common/custom_floating_button.dart';
+import 'package:notes_with_riverpod/widgets/notes/add_note_sheet.dart';
+import 'package:notes_with_riverpod/widgets/notes/bottom_navigation_widget.dart';
+import 'package:notes_with_riverpod/widgets/notes/notes_grid_view.dart';
 
-class NotesPage extends StatefulWidget {
+class NotesPage extends ConsumerWidget {
   const NotesPage({super.key});
 
   @override
-  State<NotesPage> createState() => _NotesPageState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final viewModeValue = ref.watch(notesViewModeProvider).value;
+    final viewFilteredValue = ref.watch(filterNotesProvider).value;
 
-class _NotesPageState extends State<NotesPage> {
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
       extendBody: true,
       appBar: AppBar(
         elevation: 0,
         foregroundColor: Theme.of(context).primaryColor,
         backgroundColor: Colors.transparent,
-        title: const Text('Notas'),
+        title: Text(
+          viewFilteredValue == NotesViewOptions.inbox ? 'Notas' : 'Arquivadas',
+        ),
       ),
       body: const Padding(
         padding: EdgeInsets.only(
@@ -24,7 +33,30 @@ class _NotesPageState extends State<NotesPage> {
           left: 12,
           right: 12,
         ),
-        child: Center(),
+        child: NotesGridView(),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      floatingActionButton: CustomFloatingButton(
+        onPressed: () => showModalBottomSheet(
+          context: context,
+          isScrollControlled: true,
+          backgroundColor: Colors.transparent,
+          builder: (_) => const AddNoteSheet(),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationWidget(
+        buttons: [
+          IconButton(
+            tooltip: viewModeValue.tooltip,
+            icon: viewFilteredValue.icon,
+            onPressed: () => ref.read(notesViewModeProvider.notifier).toggle(),
+          ),
+          IconButton(
+            tooltip: viewFilteredValue.tooltip,
+            icon: viewFilteredValue.icon,
+            onPressed: () => ref.read(filterNotesProvider.notifier).toggle(),
+          ),
+        ],
       ),
     );
   }
